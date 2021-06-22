9d17b5a5e2233cd92a38beb5f7423406482dc5bb
srijibbose <srijibbose.gmail.com>
2021-06-22 19:23:43 +0530

    index page initial commit

--- src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+        System.out.println("uu");
         return "index";
     }
 }
