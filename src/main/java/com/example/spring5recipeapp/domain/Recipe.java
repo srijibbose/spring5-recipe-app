bec70bcbe093835e861f382ed913b30d26d0964b
srijibbose <srijibbose.gmail.com>
2021-06-23 10:55:49 +0530

    enum added

--- src/main/java/com/example/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/example/spring5recipeapp/domain/Recipe.java
@@ -21,7 +21,9 @@ public class Recipe {
     private Set<Ingredient> ingredients;
     @Lob
     private Byte[] image;
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -104,5 +106,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
