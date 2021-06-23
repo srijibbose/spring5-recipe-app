3b9abbed3b28423d27f9f5015bd0b09ed32f4b93
srijibbose <srijibbose.gmail.com>
2021-06-23 10:49:59 +0530

    index page initial commit

--- src/main/java/com/example/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/example/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.example.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,6 +17,8 @@ public class Recipe {
     private String url;
     private String directions;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     @Lob
     private Byte[] image;
     //private Difficulty difficulty;
